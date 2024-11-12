a5585e6c1fe56225101250d3705ee7086475c980
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-11-12 08:12:21 -0600

    Create page1.php

---
+++ page1.php
@@ -0,0 +1,17 @@
+<h1>Do People Like Haya?</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+data = {
+    datasets: [{
+        data: [2, 10]
+    }],
+
+    labels: [
+        'No',
+        'Not at all'
+    ]
+};
+ 

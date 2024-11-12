08217248f275750ffc0cc9eaf61a89f9f487d37b
lakhani-haya <157073204+lakhani-haya@users.noreply.github.com>
2024-11-12 09:02:14 -0600

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 </head>
 <body>
     <h1>Welcome to Statistics about Haya</h1>
-    <p>Explore statistics about Haya using interactive charts:</p>
     <ul>
         <li><a href="page1.php">Do People Like Haya? </a></li>
         <li><a href="page2.php">Cities Haya Has Visited by Continent </a></li>
